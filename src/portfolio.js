6cd8d0aef94f8fadae59caf7d9001f8f6226c954
Alydn62 <muhamadaliyudin62@gmail.com>
2024-11-27 20:41:53 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -68,7 +68,7 @@ const projects = [
   {
     name: 'Portfolio',
     description: 'I am a web developer with expertise in Node.js, building fast, scalable, and efficient web applications. With experience in backend development and APIs, I create robust and reliable digital solutions.',
-    stack: ['JavaScript 69.6%', 'CSS 27.9%', 'HTML2.5%'],
+    stack: ['JavaScript 69.5%', 'CSS 27.8%', 'HTML2.7%'],
     sourceCode: 'https://github.com/Alydn62/Aliyudin-Portfolio',
     livePreview: 'https://alydn62.github.com/Aliyudin-Portfolio',
     image: Portfolio1,
