1426bbb1a580a77de19f64850b8d9657cb5225a6
Alydn62 <muhamadaliyudin62@gmail.com>
2025-01-04 19:51:55 +0700

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import uniqid from 'uniqid';
 import { projects } from '../../portfolio';
 import ProjectContainer from '../ProjectContainer/ProjectContainer';
 import './Projects.css';
 
 const Projects = () => {
-  if (!projects.length) return null;
+  const [category, setCategory] = useState('SEMUA');  // Menyimpan kategori yang dipilih
+
+  // Filter proyek berdasarkan kategori yang dipilih
+  const filteredProjects = category === 'SEMUA' 
+    ? projects 
+    : projects.filter(project => project.category === category);
 
   return (
     <section id='projects' className='section projects'>
       <h2 className='section__title'>Projects</h2>
 
+      <div className="filter-buttons">
+  <button type="button" onClick={() => setCategory('SEMUA')}>Semua</button>
+  <button type="button" onClick={() => setCategory('TUGAS')}>Tugas</button>
+  <button type="button" onClick={() => setCategory('NONTUGAS')}>Nontugas</button>
+</div>
+
       <div className='projects__grid'>
-        {projects.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectContainer key={uniqid()} project={project} />
         ))}
       </div>
